bdbafaeeef94ae0beaa75591fba7fe74f28a28b8
UnkAtreus <unk.atreus@gmail.com>
2021-11-20 18:14:55 +0700

    feat: add jest for test spec

--- src/module/Home/index.tsx
+++ src/module/Home/index.tsx
@@ -3,17 +3,19 @@ import React from 'react';
 import { Layout } from 'antd';
 import { Content, Footer } from 'antd/lib/layout/layout';
 
-import Star from 'common/assets/images/star.svg';
+import StarIcon from 'common/assets/images/star.svg';
 import Navbar from 'common/components/Navbar';
 
 import BannerSlider from './components/BannerSlider';
 import DiscoverMovie from './components/DiscoverMovie';
 import News from './components/News';
 
+const Star: React.FC = () => <StarIcon className="absolute h-full w-full z-0" />;
+
 const Home: React.FC = () => {
   return (
     <Layout className=" min-h-screen h-full">
-      <Star className="absolute h-full w-full z-0" />
+      <Star />
       <Navbar />
       <Content>
         <BannerSlider />
