337775a09c8de65868870f2ca51aecc62d4ecd02
Karn12003 <nawakarnmek@gmail.com>
2021-11-24 18:57:34 +0700

    feat: add nav dropdown and avatar

--- src/common/components/Layouts/Navbar/index.tsx
+++ src/common/components/Layouts/Navbar/index.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Menu, Button } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { Menu, Button, Avatar, Dropdown, message, Divider } from 'antd';
 import { Header } from 'antd/lib/layout/layout';
 import { useRouter } from 'next/dist/client/router';
+import Link from 'next/link';
 
 import Logo from 'common/assets/images/logoNav.svg';
+
+const menu = (
+  <Menu className="rounded-lg">
+    <Menu.Item key="1" className="text-primary-default">
+      My profile
+    </Menu.Item>
+    <Menu.Item key="2" className="text-primary-default">
+      Request Movie
+    </Menu.Item>
+    <Menu.Divider className="bg-primary-default w-40 mx-auto " />
+    <Menu.Item key="3" className="text-primary-default">
+      Log out
+    </Menu.Item>
+  </Menu>
+);
 
 const NAV_CONTENT = [
   { key: 'home', title: 'Home' },
@@ -13,6 +30,7 @@ const NAV_CONTENT = [
 ];
 
 const Navbar: React.FC<{ router?: string[] }> = ({ router }) => {
+  const [isAuth, setIsAuth] = useState(true);
   const Router = useRouter();
 
   return (
@@ -40,12 +58,29 @@ const Navbar: React.FC<{ router?: string[] }> = ({ router }) => {
           ))}
         </Menu>
         <div className=" flex h-16 items-center">
-          <Button
-            className="px-6 py-0 order-5 mr-6 font-poppins "
-            type="primary"
-            shape="round">
-            Login or Register
-          </Button>
+          {isAuth ? (
+            <div className="flex gap-5">
+              <Link href="/profile">
+                <a className="cursor-pointer">Nawa Lee</a>
+              </Link>
+              <Dropdown overlay={menu}>
+                <div className="flex gap-x-4  items-center flex-1 ">
+                  <Avatar
+                    src="https://joeschmoe.io/api/v1/random"
+                    size={54}
+                    className="border-2 border-primary-default"
+                  />
+                </div>
+              </Dropdown>
+            </div>
+          ) : (
+            <Button
+              className="px-6 py-0 order-5 mr-6 font-poppins "
+              type="primary"
+              shape="round">
+              Login or Register
+            </Button>
+          )}
         </div>
       </div>
     </Header>
